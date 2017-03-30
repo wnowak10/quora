baaa0191baba1a7c2ab806c554237a636072f5ff
wnowak <wnowak10@gmail.com>
2017-03-30 10:39:26 -0400

    cleaned code some

--- create_features.py
+++ create_features.py
@@ -1,6 +1,6 @@
 
 from import_data import df_train
-from import_data import df_test
+# from import_data import df_test
 
 import pandas as pd
 import numpy as np
@@ -47,55 +47,45 @@ def apply_and_add_function(df, function, col1, col2, newname1, newname2):
 print('finding word counts')
 stops = set(stopwords.words("english"))
 
-
-def word_and_punct_count(row):
+def word_count(row):
     '''
-    word count -- Return a count for
-    the number of words and
-    punctuation marks in each question.
-
-    split the text using nltk tokenize.
-    each symbol is its own 'word'...including
-    . and ,'s and such
+    split each string and return length
     '''
-    try:
-        word_and_punct_count = wordpunct_tokenize(row)
-        # just return the length
-        return(len(word_and_punct_count))
-    except:
-        pass
+    l = len(str(row).split())
+    return l
 
 
 tqdm.pandas()
 df_train = apply_and_add_function(df_train,
-                                  word_and_punct_count,
-                                  'question1', 'question2',
-                                  'q1 word count', 'q2 word count')
+                                  word_count,
+                                  'question1',
+                                  'question2',
+                                  'q1 word count',
+                                  'q2 word count')
 
 
-tqdm.pandas()
-df_test = apply_and_add_function(df_test,
-                                 word_and_punct_count,
-                                 'question1',
-                                 'question2',
-                                 'q1 word count',
-                                 'q2 word count')
-
+# tqdm.pandas()
+# df_test = apply_and_add_function(df_test,
+#                                  word_count,
+#                                  'question1',
+#                                  'question2',
+#                                  'q1 word count',
+#                                  'q2 word count')
 
 print('completed function 1 for train and test')
 
-############################
-####### Function 2. ######## 
-############################
+# ############################
+# ####### Function 2. ######## 
+# ############################
 
-# cache stop words
+# # cache stop words
 cachedStopWords = stopwords.words("english")
 
 
 def return_words(row):
     '''this function just returns each word in a question'''
     try:
-        words = wordpunct_tokenize(row)
+        words = str(row).split()
         text = [word for word in words if word not in cachedStopWords]
         lower_text = [word.lower() for word in text]
         return(lower_text)
@@ -111,36 +101,21 @@ df_train = apply_and_add_function(df_train,
                                   'q1words',
                                   'q2words')
 
-
-tqdm.pandas()
-df_test = apply_and_add_function(df_test,
-                                 return_words,
-                                 'question1',
-                                 'question2',
-                                 'q1words',
-                                 'q2words')
+# tqdm.pandas()
+# df_test = apply_and_add_function(df_test,
+#                                  return_words,
+#                                  'question1',
+#                                  'question2',
+#                                  'q1words',
+#                                  'q2words')
 
 print('completed function 2 for train and test')
 
 
-# ############################
-# ####### Function 3. ######## 
-# ############################
+# # ############################
+# # ####### Function 3. ######## 
+# # ############################
 
-
-# def number_shared_words(row):
-#     '''
-#     written by ME
-#     how many words, in total,
-#     are shared btw two questions
-#     '''
-#     try:
-#         q1 = row['q1words']
-#         q2 = row['q2words']
-#         num_common_words = sum(pd.Series(q1).isin(q2))
-#         return(num_common_words)
-#     except:
-#         pass
 
 def word_match_share(row):
     '''
@@ -165,36 +140,15 @@ def word_match_share(row):
     return R
 
 df_train['word_match_share'] = df_train.progress_apply(word_match_share, axis=1)
-df_test['word_match_share'] = df_test.progress_apply(word_match_share, axis=1)
+# df_test['word_match_share'] = df_test.progress_apply(word_match_share, axis=1)
 
-
-
-# mine
-# df_train['number shared words'] = df_train.progress_apply(number_shared_words, axis=1)
-# df_test['number shared words'] = df_test.progress_apply(number_shared_words, axis=1)
 
 print('completed function 3 for train and test')
 
 
-############################
-####### Function 4. ######## 
-############################
-
-
-# new feature -- what fraction of question
-# lengths (on average) is shared btw questions
-# df_train['percent shared words'] = \
-#     ((2 * df_train['number shared words']) /
-#      (df_train['q1 word count'] + df_train['q2 word count']))
-
-# df_test['percent shared words'] = \
-#     ((2 * df_test['number shared words']) /
-#      (df_test['q1 word count'] + df_test['q2 word count']))
-
-# print('completed feature 4 for train and test')
 
 ###########################
-###### Function 5. ######## 
+###### Function 4. ######## 
 ###########################
 
 
@@ -212,14 +166,14 @@ def how_match(row):
 
 # tqdm.pandas()
 df_train['how match?'] = df_train.progress_apply(how_match, axis=1)
-# tqdm.pandas()
-df_test['how match?'] = df_test.progress_apply(how_match, axis=1)
+# # tqdm.pandas()
+# df_test['how match?'] = df_test.progress_apply(how_match, axis=1)
 
-print('completed function 5 for train and test')
+# print('completed function 5 for train and test')
 
-############################
-####### Function 6. ######## 
-############################
+# ############################
+# ####### Function 6. ######## 
+# ############################
 
 
 def what_match(row):
@@ -234,17 +188,17 @@ def what_match(row):
         pass
 
 
-# tqdm.pandas()
+tqdm.pandas()
 df_train['what match?'] = df_train.progress_apply(what_match, axis=1)
-# tqdm.pandas()
-df_test['what match?'] = df_test.progress_apply(what_match, axis=1)
+# # tqdm.pandas()
+# df_test['what match?'] = df_test.progress_apply(what_match, axis=1)
 
 
-print('completed function 6 for train and test')
+# print('completed function 6 for train and test')
 
-############################
-####### Function 7. ######## 
-############################
+# ############################
+# ####### Function 5. ######## 
+# ############################
 
 
 def where_match(row):
@@ -258,16 +212,16 @@ def where_match(row):
     except:
         pass
 
-
+tqdm.pandas()
 df_train['where match?'] = df_train.progress_apply(where_match, axis=1)
-df_test['where match?'] = df_test.progress_apply(where_match, axis=1)
+# df_test['where match?'] = df_test.progress_apply(where_match, axis=1)
 
 
-print('completed function 7 for train and test')
+# print('completed function 7 for train and test')
 
-############################
-####### Function 8. ######## 
-############################
+# ############################
+# ####### Function 6. ######## 
+# ############################
 
 
 def why_match(row):
@@ -281,22 +235,24 @@ def why_match(row):
     except:
         pass
 
+tqdm.pandas()
 df_train['why match?'] = df_train.progress_apply(why_match, axis=1)
-df_test['why match?'] = df_test.progress_apply(why_match, axis=1)
+# df_test['why match?'] = df_test.progress_apply(why_match, axis=1)
 
-print('completed function 8 for train and test')
+# print('completed function 8 for train and test')
 
 
 ############################
-####### Function 9.0 - TF IDF for train ######## 
+####### Function 7 - TF IDF ######## 
 ############################
 
-### Use Anokas code instead of nltk...
+# ### Use Anokas code instead of nltk...
 
 train_qs = pd.Series(df_train['question1'].tolist() +
                      df_train['question2'].tolist()).astype(str)
-test_qs = pd.Series(df_test['question1'].tolist() +
-                    df_test['question2'].tolist()).astype(str)
+# test_qs = pd.Series(df_test['question1'].tolist() +
+#                     df_test['question2'].tolist()).astype(str)
+
 
 # If a word appears only once, we ignore it completely (likely a typo)
 # Epsilon defines a smoothing constant, which makes the effect of extremely rare words smaller
@@ -315,10 +271,13 @@ stops = stopwords.words("english")
 
 # handle np.warning that results from division by NA 
 # (i assume that is the problem)
-
 warnings.filterwarnings("error")
 
 def tfidf_word_match_share(row):
+    '''
+    from anokas to find td idf score for each word
+    then divide sum of shared td idf by total td idf weights
+    '''
     q1words = {}
     q2words = {}
     for word in str(row['question1']).lower().split():
@@ -342,89 +301,25 @@ def tfidf_word_match_share(row):
 
 tqdm.pandas()
 df_train['td idf '] = df_train.progress_apply(tfidf_word_match_share, axis=1, raw=True)
-tqdm.pandas()
-df_test['td idf '] = df_test.progress_apply(tfidf_word_match_share, axis=1, raw=True)
-
-
-
-
-
-
-#####
-## ME
-####
-
-# # use itertools to gather all words in a list
-# q1words_set = list(chain.from_iterable(df_train['q1words']))
-# q2words_set = list(chain.from_iterable(df_train['q2words']))
-# # combine list 1 and list 2. use list, not set because we want duplicates!
-# all_words = q1words_set + q2words_set
-# # len(all_words)
-
-# vectorizer = TfidfVectorizer(min_df=1)
-# X = vectorizer.fit_transform(all_words)
-# idf = vectorizer.idf_
-# td_idfs = dict(zip(vectorizer.get_feature_names(), idf))
-
-
-# def number_shared_words_ididf(row):
-#     try:
-#         q1 = row['q1words']
-#         q2 = row['q2words']
-#         common_words_index = pd.Series(q1).isin(q2)
-#         shared = list(compress(q1, common_words_index))
-#         summm = 0
-#         for k in shared:
-#             if k in td_idfs:
-#                 summm += td_idfs[k]
-#         return(summm)
-#     except:
-#         pass
-
-# # tqdm.pandas()
-# df_train['td-idf shared word score'] = df_train.progress_apply(number_shared_words_ididf, axis=1)
-
-# print(df_train.head())
-# ############################
-# ####### Function 9.1 - TF IDF for test set ######## 
-# ############################
-
-
-# q1words_set_test = list(chain.from_iterable(df_test['q1words']))
-# q2words_set_test = list(chain.from_iterable(df_test['q2words']))
-# # combine list 1 and list 2. use list, not set because we want duplicates!
-# all_words_test = q1words_set_test + q2words_set_test
-# len(all_words_test)
-
-
-# # from sklearn.feature_extraction.text import TfidfVectorizer
-# vectorizer_test = TfidfVectorizer(min_df=1)
-# Y = vectorizer_test.fit_transform(all_words_test)
-# idf_test = vectorizer_test.idf_
-# td_idfs_test = dict(zip(vectorizer_test.get_feature_names(), idf))
-
-
-# def number_shared_words_ididf(row):
-#     try:
-#         q1 = row['q1words']
-#         q2 = row['q2words']
-#         common_words_index = pd.Series(q1).isin(q2)
-#         shared = list(compress(q1, common_words_index))
-#         summm = 0
-#         for k in shared:
-#             if k in td_idfs_test:
-#                 summm += td_idfs_test[k]
-#         return(summm)
-#     except:
-#         pass
-
 # tqdm.pandas()
-# df_test['td-idf shared word score'] = df_test.progress_apply(number_shared_words_ididf, axis=1)
+# df_test['td idf '] = df_test.progress_apply(tfidf_word_match_share, axis=1, raw=True)
 
-print('creating features done!')
+############################
+####### Feature 8 - TF IDF ######## 
+############################
 
-print('testcols', df_test.columns.values)
+
+############################
+####### Done ######## 
+############################
+
+print('creating features done! create features csv')
+
+df_train.to_csv('df_train.csv', index=False)
+
+
+# print('testcols', df_test.columns.values)
 print('traincols', df_train.columns.values)
 
-print('testshape', df_test.shape)
+# print('testshape', df_test.shape)
 print('train shape', df_train.shape)
