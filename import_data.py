887d1490f1c6678125e227f51e82ea12ee281994
wnowak <wnowak10@gmail.com>
2017-04-01 19:42:47 -0400

    smaller import?

--- import_data.py
+++ import_data.py
@@ -9,15 +9,15 @@ import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 print('reading training set')
-# df_train = pd.read_csv('df_train.csv',
-# 						header=0,
-# 						sep=',',
-# 						quotechar='"')
+df_train = pd.read_csv('small_df_train.csv',
+						header=0,
+						sep=',',
+						quotechar='"')
 
-# df_test = pd.read_csv('df_test.csv',
-# 						header=0,
-# 						sep=',',
-# 						quotechar='"')
+df_test = pd.read_csv('small_df_test.csv',
+						header=0,
+						sep=',',
+						quotechar='"')
 
 
 # read test and train from kaggle
